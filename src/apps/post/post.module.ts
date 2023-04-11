9317334bc09d84f06b05623acb81488ba970c344
maiductrung123 <trungmd@tokyotechlab.com>
2023-04-11 23:44:21 +0700

    -n

---
+++ src/apps/post/post.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+
+@Module({
+  import: [],
+  controllers: [],
+})
+export class PostModule {}
---
+++ src/apps/post/service/post.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, Inject } from '@nestjs/common';
+@Injectable()
+export class PostService {
+  constructor(@Inject('CLOUDINARY') private readonly cloudinary) {}
+
+  async uploadImage(url: string, publicId: string): Promise<any> {
+    const options = { public_id: publicId };
+    const result = await this.cloudinary.uploader.upload(url, options);
+    return result;
+  }
+
+  generateImageUrl(publicId: string, width: number, height: number): string {
+    const options = {
+      width: width,
+      height: height,
+      crop: 'fill',
+    };
+    return this.cloudinary.url(publicId, options);
+  }
+}
