38df103d1547d0756401af9b4943eaf063ea0f09
interns.trimv <maivantri309@gmail.com>
2023-04-12 20:00:35 +0700

    change password

--- src/apps/account/controller/account.controller.ts
+++ src/apps/account/controller/account.controller.ts
@@ -1,5 +1,6 @@
-import { Get,Controller, Request } from "@nestjs/common";
+import { Get,Controller, Request, Post, Body } from "@nestjs/common";
 import { AccountService } from "../service/account.service";
+import { ChangePasswordRequest } from "src/core/dto/account/change-password.dto";
 
 
 
@@ -11,5 +12,10 @@ export class AccountController{
     getMyProfile(@Request() request){
       const userDecode=request.user;
       return this.accountService.findByUsername(userDecode.username);
+    }
+
+    @Post("/change-password")
+    changePassword(@Body() changePasswordRequest:ChangePasswordRequest,@Request() request){
+          return this.accountService.changePassword(request.userId,changePasswordRequest.oldPassword,changePasswordRequest.newPasswrod);
     }
 }
